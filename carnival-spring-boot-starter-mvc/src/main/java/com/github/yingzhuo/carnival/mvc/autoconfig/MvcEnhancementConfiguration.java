708915eefa4973619b17147ac44c304014dd7d45
应卓 <yingzhor@gmail.com>
2018-06-06 09:24:53 +0800

    update

--- carnival-spring-boot-starter-mvc/src/main/java/com/github/yingzhuo/carnival/mvc/autoconfig/MvcEnhancementConfiguration.java
+++ carnival-spring-boot-starter-mvc/src/main/java/com/github/yingzhuo/carnival/mvc/autoconfig/MvcEnhancementConfiguration.java
@@ -11,13 +11,16 @@ package com.github.yingzhuo.carnival.mvc.autoconfig;
 
 import com.github.yingzhuo.carnival.mvc.support.IpHandlerMethodArgumentResolver;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.Ordered;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
 import org.springframework.web.util.UrlPathHelper;
 
 import java.util.List;
@@ -26,6 +29,13 @@ import java.util.List;
 @EnableConfigurationProperties(MvcEnhancementConfiguration.Props.class)
 @ConditionalOnProperty(prefix = "carnival.mvc.enhancement", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class MvcEnhancementConfiguration implements WebMvcConfigurer {
+
+    @Autowired(required = false)
+    public void config(BeanNameViewResolver resolver) {
+        if (resolver != null) {
+            resolver.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        }
+    }
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
