ede9637a78670ce9aa651e23b154624d9fa4074c
应卓 <yingzhor@gmail.com>
2018-06-26 08:19:02 +0800

    update

---
+++ carnival-spring-boot-starter-redis-distributed-lock/src/main/java/com/github/yingzhuo/carnival/redis/distributed/lock/exception/CannotGetLockException.java
@@ -0,0 +1,31 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.redis.distributed.lock.exception;
+
+/**
+ * @author 应卓
+ */
+public class CannotGetLockException extends RuntimeException {
+
+    private String key;
+
+    public CannotGetLockException() {
+        this(null);
+    }
+
+    public CannotGetLockException(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+}
