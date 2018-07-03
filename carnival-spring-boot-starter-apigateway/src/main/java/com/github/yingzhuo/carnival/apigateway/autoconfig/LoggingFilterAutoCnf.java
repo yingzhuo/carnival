342fbc174dfa02de5a30f26c98fead29f1098b7e
应卓 <yingzhor@gmail.com>
2018-07-03 13:17:14 +0800

    update

---
+++ carnival-spring-boot-starter-apigateway/src/main/java/com/github/yingzhuo/carnival/apigateway/autoconfig/LoggingFilterAutoCnf.java
@@ -0,0 +1,29 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.apigateway.autoconfig;
+
+import com.github.yingzhuo.carnival.apigateway.filter.LoggingFilter;
+import com.github.yingzhuo.carnival.common.condition.ConditionalOnDebugMode;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author 应卓
+ */
+public class LoggingFilterAutoCnf {
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnDebugMode
+    public LoggingFilter loggingFilter() {
+        return new LoggingFilter();
+    }
+
+}
---
+++ carnival-spring-boot-starter-apigateway/src/main/java/com/github/yingzhuo/carnival/apigateway/filter/LoggingFilter.java
@@ -0,0 +1,66 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.apigateway.filter;
+
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+@Slf4j
+public class LoggingFilter extends AbstractZuulFilter {
+
+    @Override
+    protected boolean doShouldFilter(RequestContext requestContext) {
+        return true;
+    }
+
+    @Override
+    protected void doRun(RequestContext requestContext) throws ZuulException {
+
+        try {
+            doLog(requestContext.getRequest());
+        } catch (Exception e) {
+            // NOP
+        }
+
+    }
+
+    private void doLog(HttpServletRequest request) {
+        log.debug(StringUtils.repeat('-', 120));
+
+        log.debug("[Path]: ");
+        log.debug("\t\t\t{}", request.getRequestURI());
+
+        log.debug("[Method]: ");
+        log.debug("\t\t\t{}", request.getMethod());
+
+        log.debug("[Headers]: ");
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            String value = request.getHeader(name);
+            log.debug("\t\t\t{} = {}", name, name.equalsIgnoreCase("cookie") ? StringUtils.abbreviate(value, 60) : value);
+        }
+
+        log.debug("[Params]: ");
+        Enumeration<String> paramNames = request.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            String name = paramNames.nextElement();
+            String value = request.getParameter(name);
+            log.debug("\t\t\t{} = {}", name, value);
+        }
+
+        log.debug(StringUtils.repeat('-', 120));
+    }
+}
