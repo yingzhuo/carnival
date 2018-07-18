ce3b66707548a21931c5f709c2fd8e9bf213b416
应卓 <yingzhor@gmail.com>
2018-07-18 18:06:08 +0800

    update

---
+++ carnival-spring-boot-starter-springutils/src/main/java/com/github/yingzhuo/carnival/spring/ConversionUtils.java
@@ -0,0 +1,35 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.spring;
+
+import lombok.val;
+import org.springframework.core.convert.TypeDescriptor;
+
+/**
+ * @author 应卓
+ */
+public final class ConversionUtils {
+
+    private ConversionUtils() {
+        super();
+    }
+
+    public static <T> T convert(Object source, Class<T> targetType) {
+        val service = SpringUtils.getConversionService();
+        return service.convert(source, targetType);
+    }
+
+    @Deprecated
+    public static Object convert(Object source, TypeDescriptor sourceType, TypeDescriptor targetType) {
+        val service = SpringUtils.getConversionService();
+        return service.convert(source, sourceType, targetType);
+    }
+
+}
