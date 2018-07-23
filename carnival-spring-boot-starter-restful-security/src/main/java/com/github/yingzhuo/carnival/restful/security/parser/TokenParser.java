c60edc2fcaf7847149a3a953a8d13cbc6734127c
应卓 <yingzhor@gmail.com>
2018-07-23 08:06:53 +0800

    update

--- carnival-spring-boot-starter-restful-security/src/main/java/com/github/yingzhuo/carnival/restful/security/parser/TokenParser.java
+++ carnival-spring-boot-starter-restful-security/src/main/java/com/github/yingzhuo/carnival/restful/security/parser/TokenParser.java
@@ -11,9 +11,11 @@ package com.github.yingzhuo.carnival.restful.security.parser;
 
 import com.github.yingzhuo.carnival.common.parser.Parser;
 import com.github.yingzhuo.carnival.restful.security.token.Token;
+import lombok.val;
 import org.springframework.web.context.request.NativeWebRequest;
 
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -26,5 +28,17 @@ import java.util.Optional;
 public interface TokenParser extends Parser<NativeWebRequest, Token> {
 
     public Optional<Token> parse(NativeWebRequest webRequest, Locale locale);
+
+    public default TokenParser chain(TokenParser that) {
+        return (webRequest, locale) -> {
+            val thisOp = this.parse(webRequest, locale);
+
+            if (thisOp.isPresent()) {
+                return thisOp;
+            } else {
+                return Objects.requireNonNull(that).parse(webRequest, locale);
+            }
+        };
+    }
 
 }
