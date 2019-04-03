86ed4164fa6d69dd0a4034f781f669582026cce9
yingzhuo <yingzhor@gmail.com>
2019-04-03 17:30:23 +0800

    update

---
+++ carnival-spring-boot-starter-common/src/main/java/com/github/yingzhuo/carnival/jsr349/FieldsValueNotMatch.java
@@ -0,0 +1,44 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.jsr349;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * @author 应卓
+ */
+@Repeatable(FieldsValueNotMatch.List.class)
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = FieldsValueNotMatchValidator.class)
+public @interface FieldsValueNotMatch {
+
+    public String message() default "Fields values match!";
+
+    public String field();
+
+    public String fieldMatch();
+
+    public Class<?>[] groups() default {};
+
+    public Class<? extends Payload>[] payload() default {};
+
+    @Inherited
+    @Documented
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        FieldsValueNotMatch[] value();
+    }
+
+}
---
+++ carnival-spring-boot-starter-common/src/main/java/com/github/yingzhuo/carnival/jsr349/FieldsValueNotMatchValidator.java
@@ -0,0 +1,39 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.jsr349;
+
+import org.springframework.beans.BeanWrapperImpl;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Objects;
+
+/**
+ * @author 应卓
+ */
+public class FieldsValueNotMatchValidator implements ConstraintValidator<FieldsValueNotMatch, Object> {
+
+    private String field;
+    private String fieldMatch;
+
+    @Override
+    public void initialize(FieldsValueNotMatch annotation) {
+        this.field = annotation.field();
+        this.fieldMatch = annotation.fieldMatch();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        Object fieldValue = new BeanWrapperImpl(value).getPropertyValue(field);
+        Object fieldMatchValue = new BeanWrapperImpl(value).getPropertyValue(fieldMatch);
+        return !Objects.equals(fieldValue, fieldMatchValue);
+    }
+
+}
