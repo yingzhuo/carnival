3c46759a17d1a730240405c5e0ad76658d852279
yingzhuo <yingzhor@gmail.com>
2019-04-08 20:21:36 +0800

    update

---
+++ carnival-spring-boot-starter-json/src/main/java/com/github/yingzhuo/carnival/json/Views.java
@@ -0,0 +1,23 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.json;
+
+/**
+ * @author 应卓
+ */
+public interface Views {
+
+    public static interface Public {
+    }
+
+    public static interface Internal {
+    }
+
+}
