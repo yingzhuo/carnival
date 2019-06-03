5e3e73ed123fe753943dbceb2940f220238cb61d
yingzhuo <yingzhor@gmail.com>
2019-06-04 07:10:47 +0800

    update

---
+++ carnival-spring-boot-starter-json/src/main/java/com/github/yingzhuo/carnival/json/ApiResult.java
@@ -0,0 +1,38 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.json;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 应卓
+ * @since 1.0.2
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResult<T> implements Serializable {
+
+    @Builder.Default
+    private String code = "200";
+
+    @Singular
+    @Builder.Default
+    private List<String> errorMessages = new ArrayList<>();
+
+    private T result;
+
+}
