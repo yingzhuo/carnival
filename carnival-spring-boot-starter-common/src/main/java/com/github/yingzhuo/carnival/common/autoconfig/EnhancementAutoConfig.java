9af2ffa788dcc312c76d6225d31a2dca5b232a4b
yingzhuo <yingzhor@gmail.com>
2019-09-18 14:49:38 +0800

    update

---
+++ carnival-spring-boot-starter-common/src/main/java/com/github/yingzhuo/carnival/common/autoconfig/EnhancementAutoConfig.java
@@ -0,0 +1,40 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.common.autoconfig;
+
+import lombok.val;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author 应卓
+ */
+public class EnhancementAutoConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public RestTemplate restTemplate() {
+        val bean = new RestTemplate();
+        val converters = bean.getMessageConverters();
+        for (HttpMessageConverter<?> converter : converters) {
+            if (converter instanceof StringHttpMessageConverter) {
+                val c = (StringHttpMessageConverter) converter;
+                c.setDefaultCharset(StandardCharsets.UTF_8);
+            }
+        }
+        return bean;
+    }
+
+}
