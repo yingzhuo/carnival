c6f78e89d1a9cd6fa251b80b402b7857b2009ec6
yingzhuo <yingzhor@gmail.com>
2019-12-05 12:21:03 +0800

    2019-12-05 12:21:01

---
+++ carnival-spring-boot-starter-id/src/main/java/com/github/yingzhuo/carnival/id/impl/CuratorIdStringIdGenerator.java
@@ -0,0 +1,24 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.id.impl;
+
+import com.github.yingzhuo.carnival.id.StringIdGenerator;
+
+/**
+ *
+ */
+public class CuratorIdStringIdGenerator implements StringIdGenerator {
+
+    @Override
+    public String nextId() {
+        return null;
+    }
+
+}
