6693cc4780d9a0837810bfa18a50dfdf90e9450d
yingzhuo <yingzhor@gmail.com>
2020-01-10 10:03:26 +0800

    2020-01-10 10:03:24

---
+++ carnival-spring-boot-starter-config/src/main/java/com/github/yingzhuo/carnival/config/GitEnvironmentPostProcessor.java
@@ -0,0 +1,31 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.config;
+
+import com.github.yingzhuo.carnival.config.support.AbstractConventionEnvironmentPostProcessor;
+
+/**
+ * @author 应卓
+ * @since 1.4.1
+ */
+public class GitEnvironmentPostProcessor extends AbstractConventionEnvironmentPostProcessor {
+
+    private static final String[] DEFAULT_PREFIX = {
+            "classpath:git",
+            "classpath:META-INF/git"
+    };
+
+    private static final String NAME = "git";
+
+    public GitEnvironmentPostProcessor() {
+        super(DEFAULT_PREFIX, NAME);
+    }
+
+}
