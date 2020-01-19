98d0a356c99f61154ead528b499c35fd95009f93
yingzhuo <yingzhor@gmail.com>
2020-01-19 11:27:05 +0800

    2020-01-19 11:27:03

--- carnival-spring-boot-starter-json/src/main/java/com/github/yingzhuo/carnival/json/autoconfig/SpringDataModuleAutoConfig.java
+++ carnival-spring-boot-starter-json/src/main/java/com/github/yingzhuo/carnival/json/autoconfig/SpringDataModuleAutoConfig.java
@@ -9,12 +9,10 @@
  */
 package com.github.yingzhuo.carnival.json.autoconfig;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.yingzhuo.carnival.json.module.SpringDataJacksonModule;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-
-import java.util.Optional;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author 应卓
@@ -22,9 +20,11 @@ import java.util.Optional;
 @ConditionalOnClass(name = "org.springframework.data.domain.Page")
 public class SpringDataModuleAutoConfig {
 
-    @Autowired(required = false)
-    public void config(ObjectMapper om) {
-        Optional.ofNullable(om).ifPresent(it -> it.registerModule(new SpringDataJacksonModule()));
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> {
+            builder.modules(new SpringDataJacksonModule());
+        };
     }
 
 }
