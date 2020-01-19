933932f50838aa35070b6877738f8a6ecf653f4a
yingzhuo <yingzhor@gmail.com>
2020-01-19 16:54:09 +0800

    2020-01-19 16:54:06

--- carnival-spring-boot-starter-common/src/main/java/com/github/yingzhuo/carnival/common/datamodel/DateTimeFormat.java
+++ carnival-spring-boot-starter-common/src/main/java/com/github/yingzhuo/carnival/common/datamodel/DateTimeFormat.java
@@ -17,6 +17,9 @@ import org.springframework.format.Parser;
 import org.springframework.format.Printer;
 
 import java.lang.annotation.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.*;
 
 /**
@@ -44,6 +47,8 @@ public @interface DateTimeFormat {
             "yyyy/MM/dd",
             "yyyy/MM/dd HH:mm:ss",
             "yyyy/MM/dd HH:mm:ss.SSS",
+            "yyyy-MM",
+            "MM-dd"
     };
 
     public static class FormatterFactory implements AnnotationFormatterFactory<DateTimeFormat> {
@@ -51,9 +56,15 @@ public @interface DateTimeFormat {
         private static final Set<Class<?>> FIELD_TYPES;
 
         static {
-            val types = new HashSet<Class<?>>();
+            final Set<Class<?>> types = new HashSet<>();
             types.add(Date.class);
             types.add(Calendar.class);
+            types.add(LocalDate.class);
+            types.add(LocalDateTime.class);
+            types.add(LocalTime.class);
+            types.add(ZonedDateTime.class);
+            types.add(YearMonth.class);
+            types.add(MonthDay.class);
             FIELD_TYPES = Collections.unmodifiableSet(types);
         }
 
@@ -69,16 +80,131 @@ public @interface DateTimeFormat {
 
         @Override
         public Parser<?> getParser(DateTimeFormat annotation, Class<?> fieldType) {
-            return (text, locale) -> {
-                val date = DateUtils.parseDate(text, annotation.patterns());
+            val patterns = annotation.patterns();
 
+            return (text, locale) -> {
                 if (ClassUtils.isAssignable(fieldType, Date.class)) {
-                    return date;
+                    return DateUtils.parseDate(text, annotation.patterns());
+                } else if (ClassUtils.isAssignable(fieldType, Calendar.class)) {
+                    return DateUtils.toCalendar(DateUtils.parseDate(text, annotation.patterns()));
+                } else if (ClassUtils.isAssignable(fieldType, LocalDateTime.class)) {
+                    return parseLocalDateTime(patterns, text);
+                } else if (ClassUtils.isAssignable(fieldType, LocalDate.class)) {
+                    return parseLocalDate(patterns, text);
+                } else if (ClassUtils.isAssignable(fieldType, LocalTime.class)) {
+                    return parseLocalTime(patterns, text);
+                } else if (ClassUtils.isAssignable(fieldType, ZonedDateTime.class)) {
+                    return parseZonedDateTime(patterns, text);
+                } else if (ClassUtils.isAssignable(fieldType, YearMonth.class)) {
+                    return parseYearMonth(patterns, text);
                 } else {
-                    return DateUtils.toCalendar(date);
+                    return parseMonthDay(patterns, text);
                 }
             };
         }
+
+        private LocalDateTime parseLocalDateTime(String[] patterns, String text) {
+            LocalDateTime date = null;
+            for (String pattern : patterns) {
+                try {
+                    date = LocalDateTime.parse(text, DateTimeFormatter.ofPattern(pattern));
+                } catch (DateTimeParseException ignored) {
+                    //
+                }
+            }
+
+            if (date != null) {
+                return date;
+            } else {
+                throw new DateTimeParseException("Cannot parse", text, 0);
+            }
+        }
+
+        private LocalDate parseLocalDate(String[] patterns, String text) {
+            LocalDate date = null;
+            for (String pattern : patterns) {
+                try {
+                    date = LocalDate.parse(text, DateTimeFormatter.ofPattern(pattern));
+                } catch (DateTimeParseException ignored) {
+                    //
+                }
+            }
+
+            if (date != null) {
+                return date;
+            } else {
+                throw new DateTimeParseException("Cannot parse", text, 0);
+            }
+        }
+
+        private LocalTime parseLocalTime(String[] patterns, String text) {
+            LocalTime date = null;
+            for (String pattern : patterns) {
+                try {
+                    date = LocalTime.parse(text, DateTimeFormatter.ofPattern(pattern));
+                } catch (DateTimeParseException ignored) {
+                    //
+                }
+            }
+
+            if (date != null) {
+                return date;
+            } else {
+                throw new DateTimeParseException("Cannot parse", text, 0);
+            }
+        }
+
+        private YearMonth parseYearMonth(String[] patterns, String text) {
+            YearMonth date = null;
+            for (String pattern : patterns) {
+                try {
+                    date = YearMonth.parse(text, DateTimeFormatter.ofPattern(pattern));
+                } catch (DateTimeParseException ignored) {
+                    //
+                }
+            }
+
+            if (date != null) {
+                return date;
+            } else {
+                throw new DateTimeParseException("Cannot parse", text, 0);
+            }
+        }
+
+        private MonthDay parseMonthDay(String[] patterns, String text) {
+            MonthDay date = null;
+            for (String pattern : patterns) {
+                try {
+                    date = MonthDay.parse(text, DateTimeFormatter.ofPattern(pattern));
+                } catch (DateTimeParseException ignored) {
+                    //
+                }
+            }
+
+            if (date != null) {
+                return date;
+            } else {
+                throw new DateTimeParseException("Cannot parse", text, 0);
+            }
+        }
+
+        private ZonedDateTime parseZonedDateTime(String[] patterns, String text) {
+            ZonedDateTime date = null;
+            for (String pattern : patterns) {
+                try {
+                    date = ZonedDateTime.parse(text, DateTimeFormatter.ofPattern(pattern));
+                } catch (DateTimeParseException ignored) {
+                    //
+                }
+            }
+
+            if (date != null) {
+                return date;
+            } else {
+                throw new DateTimeParseException("Cannot parse", text, 0);
+            }
+        }
+
     }
 
 }
