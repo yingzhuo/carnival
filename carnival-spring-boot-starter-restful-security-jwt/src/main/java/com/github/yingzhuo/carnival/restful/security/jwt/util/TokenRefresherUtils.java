3b741dfc09e84cce56911d5155733bab765ce4a9
yingzhuo <yingzhor@gmail.com>
2020-04-28 13:21:08 +0800

    2020-04-28 13:21:07

---
+++ carnival-spring-boot-starter-restful-security-jwt/src/main/java/com/github/yingzhuo/carnival/restful/security/jwt/util/TokenRefresherUtils.java
@@ -0,0 +1,31 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.restful.security.jwt.util;
+
+import com.github.yingzhuo.carnival.restful.security.refresher.TokenRefresher;
+import com.github.yingzhuo.carnival.restful.security.token.Token;
+import com.github.yingzhuo.carnival.spring.SpringUtils;
+
+/**
+ * @author 应卓
+ * @since 1.6.0
+ */
+public class TokenRefresherUtils {
+
+    public static Token refresh(Token oldToken) {
+        return SpringUtils.getBean(TokenRefresher.class).refresh(oldToken);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private TokenRefresherUtils() {
+    }
+
+}
---
+++ carnival-spring-boot-starter-restful-security/src/main/java/com/github/yingzhuo/carnival/restful/security/refresher/TokenRefresher.java
@@ -0,0 +1,23 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.restful.security.refresher;
+
+import com.github.yingzhuo.carnival.restful.security.token.Token;
+
+/**
+ * @author 应卓
+ * @since 1.6.0
+ */
+@FunctionalInterface
+public interface TokenRefresher {
+
+    public Token refresh(Token oldToken);
+
+}
