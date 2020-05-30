acf87e349d3b1787ee9c68efaf89b7111c37fc4d
yingzhuo <yingzhor@gmail.com>
2020-05-31 07:56:15 +0800

    2020-05-31 07:56:13

---
+++ carnival-spring-boot-starter-common/src/main/java/com/github/yingzhuo/carnival/common/autoconfig/I18nAutoConfig.java
@@ -0,0 +1,31 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.common.autoconfig;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.MessageSourceAccessor;
+
+/**
+ * @author 应卓
+ * @since 1.6.12
+ */
+public class I18nAutoConfig {
+
+    @Bean
+    @ConditionalOnBean(MessageSource.class)
+    @ConditionalOnMissingBean(MessageSourceAccessor.class)
+    public MessageSourceAccessor messageSourceAccessor(MessageSource messageSource) {
+        return new MessageSourceAccessor(messageSource);
+    }
+
+}
