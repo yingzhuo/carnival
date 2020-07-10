f739551e96d8723ab242bd92f268494c1169751f
yingzhuo <yingzhor@gmail.com>
2020-07-10 09:29:04 +0800

    2020-07-10 09:29:03

---
+++ carnival-spring-boot-starter-json/src/main/java/com/github/yingzhuo/carnival/json/autoconfig/GuavaModuleAutoConfig.java
@@ -0,0 +1,28 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.json.autoconfig;
+
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author 应卓
+ */
+public class GuavaModuleAutoConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> {
+            builder.modules(new GuavaModule());
+        };
+    }
+
+}
