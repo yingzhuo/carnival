d01615e015c42426a8f051de912e71f158dc73dd
yingzhuo <yingzhor@gmail.com>
2020-07-29 14:09:02 +0800

    2020-07-29 14:09:00

--- carnival-spring-boot-starter-config/src/main/java/com/github/yingzhuo/carnival/config/env/ConventionEnvironmentPostProcessors.java
+++ carnival-spring-boot-starter-config/src/main/java/com/github/yingzhuo/carnival/config/env/ConventionEnvironmentPostProcessors.java
@@ -19,8 +19,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
  */
 public interface ConventionEnvironmentPostProcessors {
 
-    static class PropertySource extends AbstractConventionEnvironmentPostProcessor {
-
+    static class Property extends AbstractConventionEnvironmentPostProcessor {
         @Override
         protected String getName(ConfigurableEnvironment environment, SpringApplication application) {
             return "property";
@@ -47,6 +46,60 @@ public interface ConventionEnvironmentPostProcessors {
         }
     }
 
+    static class ConfigMap extends AbstractConventionEnvironmentPostProcessor {
+        @Override
+        protected String getName(ConfigurableEnvironment environment, SpringApplication application) {
+            return "config-map";
+        }
+
+        @Override
+        protected String[] getLocationsPrefix(ConfigurableEnvironment environment, SpringApplication application) {
+            final Class<?> mainClass = application.getMainApplicationClass();
+            return new String[]{
+                    JarDir.of(mainClass).getDirAsResourceLocation("config/config-map"),
+                    JarDir.of(mainClass).getDirAsResourceLocation(".config/config-map"),
+                    JarDir.of(mainClass).getDirAsResourceLocation("_config/config-map"),
+                    JarDir.of(mainClass).getDirAsResourceLocation("config-map"),
+                    "file:config/config-map",
+                    "file:.config/config-map",
+                    "file:_config/config-map",
+                    "file:config-map",
+                    "classpath:config/config-map",
+                    "classpath:.config/config-map",
+                    "classpath:_config/config-map",
+                    "classpath:config-map",
+                    "classpath:META-INF/config-map"
+            };
+        }
+    }
+
+    static class Secret extends AbstractConventionEnvironmentPostProcessor {
+        @Override
+        protected String getName(ConfigurableEnvironment environment, SpringApplication application) {
+            return "secret";
+        }
+
+        @Override
+        protected String[] getLocationsPrefix(ConfigurableEnvironment environment, SpringApplication application) {
+            final Class<?> mainClass = application.getMainApplicationClass();
+            return new String[]{
+                    JarDir.of(mainClass).getDirAsResourceLocation("config/secret"),
+                    JarDir.of(mainClass).getDirAsResourceLocation(".config/secret"),
+                    JarDir.of(mainClass).getDirAsResourceLocation("_config/secret"),
+                    JarDir.of(mainClass).getDirAsResourceLocation("secret"),
+                    "file:config/secret",
+                    "file:.config/secret",
+                    "file:_config/secret",
+                    "file:secret",
+                    "classpath:config/secret",
+                    "classpath:.config/secret",
+                    "classpath:_config/secret",
+                    "classpath:secret",
+                    "classpath:META-INF/secret"
+            };
+        }
+    }
+
     static class Git extends AbstractConventionEnvironmentPostProcessor {
         @Override
         protected String getName(ConfigurableEnvironment environment, SpringApplication application) {
@@ -55,7 +108,7 @@ public interface ConventionEnvironmentPostProcessors {
 
         @Override
         protected String[] getLocationsPrefix(ConfigurableEnvironment environment, SpringApplication application) {
-            return new String[]{"classpath:git", "classpath:META-INF/git"};
+            return new String[]{"classpath:git", "classpath:META-INF/git", "classpath:config/git"};
         }
     }
 
