024b9df01abce95fb951a0b4adff67a21142ebbf
yingzhuo <yingzhor@gmail.com>
2020-08-06 06:08:15 +0800

    2020-08-06 06:08:13

--- carnival-spring-boot-starter-mustache/src/main/java/com/github/yingzhuo/carnival/mustache/autoconfig/MustacheAutoConfig.java
+++ carnival-spring-boot-starter-mustache/src/main/java/com/github/yingzhuo/carnival/mustache/autoconfig/MustacheAutoConfig.java
@@ -1,3 +1,12 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
 package com.github.yingzhuo.carnival.mustache.autoconfig;
 
 import com.github.yingzhuo.carnival.mustache.MustacheBean;
@@ -5,6 +14,10 @@ import com.github.yingzhuo.carnival.mustache.MustacheBeanImpl;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * @author 应卓
+ * @since 1.6.33
+ */
 public class MustacheAutoConfig {
 
     @Bean
