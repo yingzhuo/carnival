82c643221e94010fc679c46a2e34e50ce3727a39
yingzhuo <yingzhor@gmail.com>
2020-09-08 19:34:03 +0800

    2020-09-08 19:34:01

---
+++ carnival-spring-boot-starter-common/src/main/java/com/github/yingzhuo/carnival/jsr349/DateString.java
@@ -0,0 +1,35 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.jsr349;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * @author 应卓
+ * @since 1.7.7
+ */
+@Documented
+@Inherited
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = DateStringValidator.class)
+public @interface DateString {
+
+    public String pattern();
+
+    public String message() default "{com.github.yingzhuo.carnival.jsr349.DateStringValidator.message}";
+
+    public Class<?>[] groups() default {};
+
+    public Class<? extends Payload>[] payload() default {};
+
+}
---
+++ carnival-spring-boot-starter-common/src/main/java/com/github/yingzhuo/carnival/jsr349/DateStringValidator.java
@@ -0,0 +1,42 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.jsr349;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+/**
+ * @author 应卓
+ * @since 1.7.7
+ */
+public class DateStringValidator implements ConstraintValidator<DateString, CharSequence> {
+
+    private DateString annotation;
+
+    @Override
+    public void initialize(DateString constraintAnnotation) {
+        this.annotation = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
+        DateFormat dateFormat = new SimpleDateFormat(annotation.pattern());
+        try {
+            dateFormat.parse(value.toString());
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+
+}
