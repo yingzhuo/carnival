57983c6e01094bf0a8762dcd5b7c86dd5ce3d035
应卓 <yingzhor@gmail.com>
2021-06-28 18:06:50 +0800

    2021-06-28 18:06:48

---
+++ carnival-spring-boot-starter-password/src/main/java/com/github/yingzhuo/carnival/password/impl/Base32PasswordEncoder.java
@@ -0,0 +1,45 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.password.impl;
+
+import cn.hutool.core.codec.Base32;
+import com.github.yingzhuo.carnival.password.PasswordEncoder;
+import org.apache.commons.lang3.StringUtils;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author 应卓
+ * @since 1.9.7
+ */
+public class Base32PasswordEncoder implements PasswordEncoder {
+
+    private final Charset charset;
+
+    public Base32PasswordEncoder() {
+        this(StandardCharsets.UTF_8);
+    }
+
+    public Base32PasswordEncoder(Charset charset) {
+        this.charset = charset;
+    }
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return Base32.encode(rawPassword.toString(), charset);
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return StringUtils.equals(encodedPassword, encode(rawPassword));
+    }
+
+}
