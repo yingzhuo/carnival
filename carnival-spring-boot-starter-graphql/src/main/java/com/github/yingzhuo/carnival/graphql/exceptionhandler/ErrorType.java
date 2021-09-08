b6f4796d103f088ea3f0886d2ec7cc3faaa1542e
应卓 <yingzhor@gmail.com>
2021-09-08 10:56:11 +0800

    2021-09-08 10:56:10

---
+++ carnival-spring-boot-starter-graphql/src/main/java/com/github/yingzhuo/carnival/graphql/exceptionhandler/ErrorType.java
@@ -0,0 +1,59 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.graphql.exceptionhandler;
+
+import graphql.ErrorClassification;
+import graphql.GraphQLError;
+
+import java.util.Objects;
+
+/**
+ * @author 应卓
+ * @see graphql.ErrorType
+ * @see org.springframework.graphql.execution.ErrorType
+ * @since 1.10.16
+ */
+public final class ErrorType {
+
+    public static final ErrorClassification BAD_REQUEST = of("BAD_REQUEST");
+    public static final ErrorClassification UNAUTHORIZED = of("UNAUTHORIZED");
+    public static final ErrorClassification FORBIDDEN = of("FORBIDDEN");
+    public static final ErrorClassification UNAUTHORIZED_OR_FORBIDDEN = of("UNAUTHORIZED_OR_FORBIDDEN");
+    public static final ErrorClassification INTERNAL_ERROR = of("INTERNAL_ERROR");
+    public static final ErrorClassification OPERATION_NOT_SUPPORTED = of("OPERATION_NOT_SUPPORTED");
+    public static final ErrorClassification BUSINESS_ERROR = of("BUSINESS_ERROR");
+
+    private ErrorType() {
+    }
+
+    public static ErrorClassification of(final String type) {
+        return new StringErrorClassification(type);
+    }
+
+    private static class StringErrorClassification implements ErrorClassification {
+
+        private final String type;
+
+        public StringErrorClassification(String type) {
+            this.type = Objects.requireNonNull(type);
+        }
+
+        @Override
+        public Object toSpecification(GraphQLError error) {
+            return this.type;
+        }
+
+        @Override
+        public String toString() {
+            return this.type;
+        }
+    }
+
+}
