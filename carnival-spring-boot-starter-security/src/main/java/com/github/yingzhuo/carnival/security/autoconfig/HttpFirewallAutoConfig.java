2ee0d7f8d7a98be3d6ab21c36691a8c373b97579
应卓 <yingzhor@gmail.com>
2021-10-19 17:18:20 +0800

    2021-10-19 17:18:18

---
+++ carnival-spring-boot-starter-security/src/main/java/com/github/yingzhuo/carnival/security/autoconfig/HttpFirewallAutoConfig.java
@@ -0,0 +1,31 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.security.autoconfig;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+/**
+ * @author 应卓
+ * @since 1.10.31
+ */
+@ConditionalOnMissingBean(HttpFirewall.class)
+public class HttpFirewallAutoConfig {
+
+    @Bean
+    HttpFirewall httpFirewall() {
+        StrictHttpFirewall bean = new StrictHttpFirewall();
+        bean.setAllowSemicolon(true);
+        return bean;
+    }
+
+}
