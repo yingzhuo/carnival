aa7a7ce4bd044bf8a6d80b130d10fc67acae4707
应卓 <yingzhor@gmail.com>
2021-11-01 16:10:52 +0800

    2021-11-01 16:10:51

---
+++ carnival-spring-boot-starter-mvc/src/main/java/com/github/yingzhuo/carnival/mvc/upload/CopyUtils.java
@@ -0,0 +1,44 @@
+/*
+ *  ____    _    ____  _   _ _____     ___    _
+ * / ___|  / \  |  _ \| \ | |_ _\ \   / / \  | |
+ * | |    / _ \ | |_) |  \| || | \ \ / / _ \ | |
+ * | |___/ ___ \|  _ <| |\  || |  \ V / ___ \| |___
+ * \____/_/   \_\_| \_\_| \_|___|  \_/_/   \_\_____|
+ *
+ * https://github.com/yingzhuo/carnival
+ */
+package com.github.yingzhuo.carnival.mvc.upload;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+/**
+ * @author 应卓
+ * @since 1.10.33
+ */
+public final class CopyUtils {
+
+    private CopyUtils() {
+    }
+
+    public static Path copyToTemp(MultipartFile file) {
+        Objects.requireNonNull(file);
+
+        try {
+            final Path path = Files.createTempFile("", "." + FilenameUtils.getExtension(file.getOriginalFilename()))
+                    .normalize();
+            FileUtils.copyToFile(file.getInputStream(), path.toFile());
+            return path;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+}
