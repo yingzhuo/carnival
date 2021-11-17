14e542583601a938eecf8bc7eab1f7624312249e
应卓 <yingzhor@gmail.com>
2021-11-17 13:35:04 +0800

    2021-11-17 13:35:02

--- carnival-spring-boot-starter-security/src/main/java/com/github/yingzhuo/carnival/security/core/LoggingFilterSkipPredicateFactories.java
+++ carnival-spring-boot-starter-security/src/main/java/com/github/yingzhuo/carnival/security/core/LoggingFilterSkipPredicateFactories.java
@@ -10,6 +10,8 @@
 package com.github.yingzhuo.carnival.security.core;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.function.Predicate;
@@ -20,18 +22,33 @@ import java.util.function.Predicate;
  */
 public final class LoggingFilterSkipPredicateFactories {
 
+    private static final PathMatcher ANT_PATH_MATCHERS = new AntPathMatcher();
+
     public LoggingFilterSkipPredicateFactories() {
     }
 
-    public static Predicate<HttpServletRequest> userAgentMatches(final String regex) {
-        return request -> {
-            final String value = request.getHeader(HttpHeaders.USER_AGENT);
-            if (value == null) {
-                return false;
-            } else {
-                return value.matches(regex);
+    public static Predicate<HttpServletRequest> pathMatches(final String antPattern) {
+        return request -> ANT_PATH_MATCHERS.match(antPattern, request.getRequestURI());
+    }
+
+    public static Predicate<HttpServletRequest> hasHeader(final String headerName) {
+        return request -> request.getHeader(headerName) != null;
+    }
+
+    public static Predicate<HttpServletRequest> headerValueMatches(final String headerName, final String headerValueRegex) {
+        return new Predicate<HttpServletRequest>() {
+            @Override
+            public boolean test(HttpServletRequest request) {
+                String value = request.getHeader(headerName);
+                if (value == null) return false;
+                return value.matches(headerValueRegex);
             }
         };
+    }
+
+    @Deprecated
+    public static Predicate<HttpServletRequest> userAgentMatches(final String regex) {
+        return headerValueMatches(HttpHeaders.USER_AGENT, regex);
     }
 
 }
